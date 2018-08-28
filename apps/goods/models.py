d9eaf56023599b4c0f16c5dfb8c8f32b47c6583c
mata <gaoyp_1@sina.com>
2018-08-28 15:38:19 +0800

    created data tables in db

--- apps/goods/models.py
+++ apps/goods/models.py
@@ -21,7 +21,7 @@ class GoodsCategory(models.Model):
     desc = models.TextField(default='', verbose_name='类别描述', help_text='类别描述')
     category_type = models.IntegerField(choices=CATEGORY_TYPE, verbose_name='类目级别', help_text='类目级别')
     parent_category = models.ForeignKey('self', null=True, blank=True, verbose_name='父类目级', help_text='父类目级',
-                                        related_name='sub_cat')
+                                        related_name='sub_cat', on_delete=models.CASCADE)
 
     is_tab = models.BooleanField(default=False, verbose_name='是否导航', help_text='是否导航')
     add_time = models.DateTimeField(default=datetime.now, verbose_name='添加时间')
@@ -102,7 +102,7 @@ class Banner(models.Model):
     """
     轮播的商品
     """
-    goods = models.ForeignKey(Goods, verbose_name="商品")
+    goods = models.ForeignKey(Goods, verbose_name="商品", on_delete=models.CASCADE)
     image = models.ImageField(upload_to='banner', verbose_name="轮播图片")
     index = models.IntegerField(default=0, verbose_name="轮播顺序")
     add_time = models.DateTimeField(default=datetime.now, verbose_name="添加时间")
