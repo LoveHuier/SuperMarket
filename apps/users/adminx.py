699c073ca8d8b577436b8fa623b8a19691625265
mata <gaoyp_1@sina.com>
2018-08-28 16:54:39 +0800

    Configuring the xadmin management system

---
+++ apps/users/adminx.py
@@ -0,0 +1,23 @@
+# _*_coding: utf-8_*_
+import xadmin
+from xadmin import views
+from .models import VerifyCode
+
+
+class BaseSetting(object):
+    enable_themes = True
+    use_bootswatch = True
+
+
+class GlobalSettings(object):
+    site_title = "超市后台管理系统"
+    site_footer = "supermarket"
+
+
+class VerifyCodeAdmin(object):
+    list_display = ['code', 'mobile', "add_time"]
+
+
+xadmin.site.register(VerifyCode, VerifyCodeAdmin)
+xadmin.site.register(views.BaseAdminView, BaseSetting)
+xadmin.site.register(views.CommAdminView, GlobalSettings)
