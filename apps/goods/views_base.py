807a1e65cec7fbe4975f8fb12495d0d27b735baa
mata <gaoyp_1@sina.com>
2018-08-30 17:58:29 +0800

    Serialize the model with a serializer

--- apps/goods/views_base.py
+++ apps/goods/views_base.py
@@ -3,9 +3,11 @@
 import json
 
 from django.views.generic.base import View
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+# from django.forms.models import model_to_dict
+from django.core import serializers
 
-from .models import Goods
+from goods.models import Goods
 
 
 class GoodsListView(View):
@@ -18,11 +20,17 @@ class GoodsListView(View):
         json_list = []
 
         goods = Goods.objects.all()[:10]
-        for good in goods:
-            json_dict = {}
-            json_dict['name'] = good.name
-            json_dict['category'] = good.category.name
-            json_dict['market_price'] = good.market_price
-            json_list.append(json_dict)
+        # for good in goods:
+        # json_dict = {}
+        # json_dict['name'] = good.name
+        # json_dict['category'] = good.category.name
+        # json_dict['market_price'] = good.market_price
 
-        return HttpResponse(json.dumps(json_list), content_type="application/json")
+        # json_dict = model_to_dict(good)
+        # json_list.append(json_dict)
+
+        json_data = serializers.serialize('json', goods)
+        json_data = json.loads(json_data)
+
+        # return HttpResponse(json_data, content_type="application/json")
+        return JsonResponse(json_data, safe=False)
