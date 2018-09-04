e1036f00108f9c4a8f892749e30d7ed968480163
mata <gaoyp_1@sina.com>
2018-09-04 16:25:07 +0800

    use mixins.ListModelMixin and generics.GenericAPIView list goods

--- apps/goods/views.py
+++ apps/goods/views.py
@@ -1,19 +1,26 @@
 from goods.serializers import GoodsSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import mixins
+from rest_framework import generics
 
 from .models import Goods
 
 
 # Create your views here.
 
-class GoodsListView(APIView):
+class GoodsListView(mixins.ListModelMixin, generics.GenericAPIView):
     """
-    List all goods.
+    商品列表页
     """
+    queryset = Goods.objects.all()
+    serializer_class = GoodsSerializer
 
-    def get(self, request, format=None):
-        goods = Goods.objects.all()[:10]
-        goods_serializer = GoodsSerializer(goods, many=True)
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
 
-        return Response(goods_serializer.data)
+        # def get(self, request, format=None):
+        #     goods = Goods.objects.all()[:10]
+        #     goods_serializer = GoodsSerializer(goods, many=True)
+        #
+        #     return Response(goods_serializer.data)
