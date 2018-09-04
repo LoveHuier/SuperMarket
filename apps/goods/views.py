fd18391dcd937b02106a62930363417c8f50d0e2
mata <gaoyp_1@sina.com>
2018-09-04 22:07:48 +0800

    custom define pagination

--- apps/goods/views.py
+++ apps/goods/views.py
@@ -3,11 +3,23 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import mixins
 from rest_framework import generics
+from rest_framework.pagination import PageNumberPagination
 
 from .models import Goods
 
 
 # Create your views here.
+
+class GoodsPagination(PageNumberPagination):
+    # 每页显示多少个
+    page_size = 10
+    # 默认每页显示3个，可以通过传入pager1/?page=2&size=4,改变默认每页显示的个数
+    page_size_query_param = "size"
+    # 最大页数不超过10
+    max_page_size = 50
+    # 获取页码数的
+    page_query_param = "page"
+
 
 class GoodsListView(generics.ListAPIView):
     """
@@ -15,12 +27,13 @@ class GoodsListView(generics.ListAPIView):
     """
     queryset = Goods.objects.all()
     serializer_class = GoodsSerializer
+    pagination_class = GoodsPagination
 
     # def get(self, request, *args, **kwargs):
     #     return self.list(request, *args, **kwargs)
 
-        # def get(self, request, format=None):
-        #     goods = Goods.objects.all()[:10]
-        #     goods_serializer = GoodsSerializer(goods, many=True)
-        #
-        #     return Response(goods_serializer.data)
+    # def get(self, request, format=None):
+    #     goods = Goods.objects.all()[:10]
+    #     goods_serializer = GoodsSerializer(goods, many=True)
+    #
+    #     return Response(goods_serializer.data)
