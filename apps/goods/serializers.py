844bf2fcab4d9a9e1c923708f62297ff2bccc14c
mata <gaoyp_1@sina.com>
2018-09-04 15:54:56 +0800

    use modelserializer to list goods

--- apps/goods/serializers.py
+++ apps/goods/serializers.py
@@ -1,8 +1,18 @@
 # _*_coding: utf-8_*_
 from rest_framework import serializers
 
+from .models import Goods, GoodsCategory
 
-class GoodsSerializer(serializers.Serializer):
-    name = serializers.CharField(required=True, max_length=100)
-    click_num = serializers.IntegerField(default=0)
 
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = GoodsCategory
+        fields = '__all__'
+
+
+class GoodsSerializer(serializers.ModelSerializer):
+    category = CategorySerializer()
+
+    class Meta:
+        model = Goods
+        fields = "__all__"
