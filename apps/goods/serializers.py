3428c6e70f9c96fb6cbc76aed07d04f5b578df68
mata <gaoyp_1@sina.com>
2018-09-11 17:17:52 +0800

    list categorys

--- apps/goods/serializers.py
+++ apps/goods/serializers.py
@@ -4,7 +4,33 @@ from rest_framework import serializers
 from .models import Goods, GoodsCategory
 
 
+class CategorySerializer3(serializers.ModelSerializer):
+    """
+    商品类别序列化
+    """
+
+    class Meta:
+        model = GoodsCategory
+        fields = '__all__'
+
+
+class CategorySerializer2(serializers.ModelSerializer):
+    """
+    商品类别序列化
+    """
+    sub_cat = CategorySerializer3(many=True)
+
+    class Meta:
+        model = GoodsCategory
+        fields = '__all__'
+
+
 class CategorySerializer(serializers.ModelSerializer):
+    """
+    商品类别序列化
+    """
+    sub_cat = CategorySerializer2(many=True)
+
     class Meta:
         model = GoodsCategory
         fields = '__all__'
