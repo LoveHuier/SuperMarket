cbe0360a649088eee9462f85994ce62de47f8765
mata <gaoyp_1@sina.com>
2018-09-13 15:35:32 +0800

    List the category data in head.vue and use the django cors headers to resolve cross-domain issues.

--- SuperMarket/settings.py
+++ SuperMarket/settings.py
@@ -52,9 +52,11 @@ INSTALLED_APPS = [
     'captcha',
     'rest_framework',
     'django_filters',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -63,7 +65,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
+CORS_ORIGIN_ALLOW_ALL = True
 ROOT_URLCONF = 'SuperMarket.urls'
 
 TEMPLATES = [
