70fb9f3b115258df9acede9a3382293fffdd6143
mata <gaoyp_1@sina.com>
2018-12-09 16:35:18 +0800

    add private key and public key

---
+++ apps/utils/alipay.py
@@ -0,0 +1,169 @@
+# _*_coding: utf-8_*_
+
+# pip install pycryptodome
+
+from datetime import datetime
+from Crypto.PublicKey import RSA
+from Crypto.Signature import PKCS1_v1_5
+from Crypto.Hash import SHA256
+from base64 import b64encode, b64decode
+from urllib.parse import quote_plus
+from urllib.parse import urlparse, parse_qs
+from urllib.request import urlopen
+from base64 import decodebytes, encodebytes
+
+import json
+
+
+class AliPay(object):
+    """
+    支付宝支付接口
+    """
+
+    def __init__(self, appid, app_notify_url, app_private_key_path,
+                 alipay_public_key_path, return_url, debug=False):
+        self.appid = appid
+        self.app_notify_url = app_notify_url
+        # 私钥文件名路径，支持相对路径
+        self.app_private_key_path = app_private_key_path
+        self.app_private_key = None
+        self.return_url = return_url
+        with open(self.app_private_key_path) as fp:
+            # RSA放在了publickey里面
+            self.app_private_key = RSA.importKey(fp.read())
+        # 支付宝公钥，在生成链接的过程中，公钥没什么用，在验证支付宝返回的消息时，它是最主要的一个key
+        self.alipay_public_key_path = alipay_public_key_path
+        with open(self.alipay_public_key_path) as fp:
+            self.alipay_public_key = RSA.import_key(fp.read())
+
+        if debug is True:
+            # 使用沙箱的url
+            self.__gateway = "https://openapi.alipaydev.com/gateway.do"
+        else:
+            # 使用正式环境的url
+            self.__gateway = "https://openapi.alipay.com/gateway.do"
+
+    def direct_pay(self, subject, out_trade_no, total_amount, return_url=None, **kwargs):
+        biz_content = {
+            "subject": subject,
+            "out_trade_no": out_trade_no,
+            "total_amount": total_amount,
+            "product_code": "FAST_INSTANT_TRADE_PAY",
+            # "qr_pay_mode":4
+        }
+
+        biz_content.update(kwargs)
+        data = self.build_body("alipay.trade.page.pay", biz_content, return_url)
+        return self.sign_data(data)
+
+    def build_body(self, method, biz_content, return_url=None):
+        data = {
+            "app_id": self.appid,
+            "method": method,
+            "charset": "utf-8",
+            "sign_type": "RSA2",
+            "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+            "version": "1.0",
+            "biz_content": biz_content
+        }
+
+        if return_url is not None:
+            data["notify_url"] = self.app_notify_url
+            data["return_url"] = self.return_url
+
+        return data
+
+    def sign_data(self, data):
+        """
+        签名
+        :param data:
+        :return:
+        """
+        # 在sign签名之前，是不能有sign字段的
+        data.pop("sign", None)
+        # 排序后的字符串
+        unsigned_items = self.ordered_data(data)
+        unsigned_string = "&".join("{0}={1}".format(k, v) for k, v in unsigned_items)
+        sign = self.sign(unsigned_string.encode("utf-8"))
+        # ordered_items = self.ordered_data(data)
+        quoted_string = "&".join("{0}={1}".format(k, quote_plus(v)) for k, v in unsigned_items)
+
+        # 获得最终的订单信息字符串
+        signed_string = quoted_string + "&sign=" + quote_plus(sign)
+        return signed_string
+
+    def ordered_data(self, data):
+        """
+        排序
+        :param data:
+        :return:
+        """
+        complex_keys = []
+        for key, value in data.items():
+            if isinstance(value, dict):
+                complex_keys.append(key)
+
+        # 将字典类型的数据dump出来
+        for key in complex_keys:
+            data[key] = json.dumps(data[key], separators=(',', ':'))
+
+        return sorted([(k, v) for k, v in data.items()])
+
+    def sign(self, unsigned_string):
+        # 开始计算签名
+        key = self.app_private_key
+        # 生成一个签名对象
+        signer = PKCS1_v1_5.new(key)
+        signature = signer.sign(SHA256.new(unsigned_string))
+        # base64 编码，转换为unicode表示并移除回车
+        sign = encodebytes(signature).decode("utf8").replace("\n", "")
+        return sign
+
+    def _verify(self, raw_content, signature):
+        # 开始计算签名
+        key = self.alipay_public_key
+        signer = PKCS1_v1_5.new(key)
+        digest = SHA256.new()
+        digest.update(raw_content.encode("utf8"))
+        if signer.verify(digest, decodebytes(signature.encode("utf8"))):
+            return True
+        return False
+
+    def verify(self, data, signature):
+        # 验证支付宝返回的数据
+        if "sign_type" in data:
+            sign_type = data.pop("sign_type")
+        # 排序后的字符串
+        unsigned_items = self.ordered_data(data)
+        message = "&".join(u"{}={}".format(k, v) for k, v in unsigned_items)
+        return self._verify(message, signature)
+
+
+if __name__ == "__main__":
+    return_url = 'http://47.92.87.172:8000/?total_amount=0.01&timestamp=2017-08-15+17%3A15%3A13&sign=jnnA1dGO2iu2ltMpxrF4MBKE20Akyn%2FLdYrFDkQ6ckY3Qz24P3DTxIvt%2BBTnR6nRk%2BPAiLjdS4sa%2BC9JomsdNGlrc2Flg6v6qtNzTWI%2FEM5WL0Ver9OqIJSTwamxT6dW9uYF5sc2Ivk1fHYvPuMfysd90lOAP%2FdwnCA12VoiHnflsLBAsdhJazbvquFP%2Bs1QWts29C2%2BXEtIlHxNgIgt3gHXpnYgsidHqfUYwZkasiDGAJt0EgkJ17Dzcljhzccb1oYPSbt%2FS5lnf9IMi%2BN0ZYo9%2FDa2HfvR6HG3WW1K%2FlJfdbLMBk4owomyu0sMY1l%2Fj0iTJniW%2BH4ftIfMOtADHA%3D%3D&trade_no=2017081521001004340200204114&sign_type=RSA2&auth_app_id=2016080600180695&charset=utf-8&seller_id=2088102170208070&method=alipay.trade.page.pay.return&app_id=2016080600180695&out_trade_no=201702021222&version=1.0'
+    o = urlparse(return_url)
+    query = parse_qs(o.query)
+    processed_query = {}
+    ali_sign = query.pop("sign")[0]
+
+    alipay = AliPay(
+        appid="2016092300577912",  # appid一定要改
+        app_notify_url="http://projectsedus.com/",
+        app_private_key_path="../trade/keys/private_2048.txt",
+        alipay_public_key_path="../trade/keys/alipay_key_2048.txt",  # 支付宝的公钥，验证支付宝回传消息使用，不是你自己的公钥,
+        debug=True,  # 默认False,是否是debug模式
+        return_url="http://127.0.0.1:8000/"  # 支付完成后要跳转到的目标url
+    )
+
+    for key, value in query.items():
+        processed_query[key] = value[0]
+    print(alipay.verify(processed_query, ali_sign))
+
+    # url = alipay.direct_pay(
+    #     subject="测试订单",
+    #     out_trade_no="201802021113",  # 订单号每次要不一样
+    #     total_amount=100,
+    #     return_url='http://127.0.0.1:8000/'
+    # )
+    # re_url = "https://openapi.alipaydev.com/gateway.do?{data}".format(data=url)
+    # print(re_url)
